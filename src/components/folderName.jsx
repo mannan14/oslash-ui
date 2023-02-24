a1966b6e516f879cac58dbecb247638e2892ef03
Mannan Dassani <mannandassani1@gmail.com>
2023-02-24 17:35:55 +0530

    modal centered

--- src/components/folderName.jsx
+++ src/components/folderName.jsx
@@ -55,38 +55,38 @@ export default function FolderName({Foldername}){
                 <Menu.Items className="absolute top-[115px] right-[265px] z-10 rounded-lg p-1 bg-white shadow-md focus:outline-none active:outline-none">
                     <div className="flex flex-col">
                         <Menu.Item>
-                                {
-                                    ({active}) => (
-                                        <>
-                                            <button
-                                            className={`${active ? `bg-gray-100 ` :` `} flex items-center text-[#111827] hover:bg-[#F9FAFB] focus:bg-blue-100 rounded-md text-base text-left p-2 leading-5`}
-                                            href="#"
-                                            >
-                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 m-1 text-[#111827]" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                                                </svg>
-                                                <p>Edit</p> 
-                                            </button>
-                                        </>
-                                    )
-                                }
+                            {
+                                ({active}) => (
+                                    <>
+                                        <button
+                                        className={`${active ? `bg-gray-100 ` :` `} flex items-center text-[#111827] hover:bg-[#F9FAFB] focus:bg-blue-100 rounded-md text-base text-left p-2 leading-5`}
+                                        href="#"
+                                        >
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 m-1 text-[#111827]" viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                                            </svg>
+                                            <p>Edit</p> 
+                                        </button>
+                                    </>
+                                )
+                            }
                         </Menu.Item>
                         <Menu.Item>
-                                {
-                                    ({active}) => (
-                                        <>
-                                            <button
-                                            className={`${active ? `bg-gray-100 ` :` `} flex items-center text-red-500 hover:bg-[#F9FAFB] focus:bg-blue-100 rounded-md text-base text-left p-2 leading-5`}
-                                            href="#"
-                                            >   
-                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 m-1 text-red-500" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-                                                </svg>
-                                                <p>Remove</p>
-                                            </button>
-                                        </>
-                                    )
-                                } 
+                            {
+                                ({active}) => (
+                                    <>
+                                        <button
+                                        className={`${active ? `bg-gray-100 ` :` `} flex items-center text-red-500 hover:bg-[#F9FAFB] focus:bg-blue-100 rounded-md text-base text-left p-2 leading-5`}
+                                        href="#"
+                                        >   
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 m-1 text-red-500" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                                            </svg>
+                                            <p>Remove</p>
+                                        </button>
+                                    </>
+                                )
+                            } 
                         </Menu.Item>
                     </div>
                 </Menu.Items>
